255001ed18dc2a285a35066c7f4f8e3edacdd136
atakanakyildiz <atakanmembership@gmail.com>
2023-01-03 23:59:46 +0100

    Its not working yet

--- GUESS WHO/GUESS WHO.py
+++ GUESS WHO/GUESS WHO.py
@@ -1,4 +1,5 @@
 FILENAME = "characters.txt"
+FILENAME2 = "question1.txt"
 from pprint import pprint
 
 
@@ -18,31 +19,29 @@ def read_file(filename):
 
 def main():
     characters = read_file(FILENAME)
-    foo = dict()
-    mynames = []
-    property = characters[0].split(";")
-    for i in range(len(property)):
-        foo[property[i]] = mynames
+    quest1 = read_file(FILENAME2)
+
+    characters[0] = characters[0].split(";")
+
+    index = []
+    result = []
+    for elements in quest1:
+        a = 0
+        elements = elements.split("=")
+        for words in characters[0]:
+            if elements[0].replace(" ", "") == words.replace(" ", ""):
+                index.append(int(a))
+                result.append(elements[1])
+            a = a + 1
+
 
     characters = characters[1:]
     for lines in characters:
-        lines = lines.replace("\n", "")
         lines = lines.split(";")
-        for words in lines:
-            mynames.append(words)
-
-    for num_lines in range(len(characters)):
-        i = 0
-        templist = []
-        foo[property[i]] = templist
-
-        for numbers in range(9):
-            templist.append(mynames[numbers*i])
-        i = i + 1
-
-
-
-    pprint(foo)
+        for i in index:
+            for things in result:
+                if things.replace(" ", "") == lines[i].replace(" ", ""):
+                    print(lines)
     
 
 if __name__ == '__main__':
